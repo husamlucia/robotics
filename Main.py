2d89f4302064c2c55cb1fd0f346ab5cda2abde2a
samer <khsamer11@gmail.com>
2021-05-20 13:10:29 +0300

    Main is ready

---
+++ Main.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+import point_extractor.ui_input as pe
+import vtff.ui_input as vtff
+
+
+def main():
+    projects = {"vtff": vtff, "pe": pe}
+
+    input_column = [[sg.Text("Press button to run program")],
+                    [sg.Button("Video to Filtered Frames", key="vtff")],
+                    [sg.Button("Point Extractor", key="pe")]]
+
+    layout = [[sg.Column(input_column)]]
+
+    window = sg.Window("Robotics", layout)
+
+    while True:
+        event, values = window.read()
+
+        if event in projects:
+            projects[event]()
+
+
+if _name_ == "_main_":
+    main()
